465e9cba894ec1485f629399532aebbd405f23b1
Lucas da Fonseca Alves <lucasfonsecalves@gmail.com>
2022-11-15 19:51:30 -0300

    Exercício de cálculo de quadriláteros

---
+++ main.java
@@ -0,0 +1,18 @@
+public class main {
+
+
+    public static void main(String[] args){
+    //Quadrilátero
+
+    System.out.println("Exercício Quadrilátero");
+    quadrilatero.area(3);
+    quadrilatero.area(5d, 5d);
+    quadrilatero.area(7, 8, 9);
+    quadrilatero.area(5f, 5f);
+
+
+    }
+
+
+    
+}
