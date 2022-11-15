2ac363207f29eddf8514c2a69115c93a4319b0fc
Lucas da Fonseca Alves <lucasfonsecalves@gmail.com>
2022-11-15 20:15:45 -0300

    Alteração dos métodos com retorno

--- quadrilatero.java
+++ quadrilatero.java
@@ -1,34 +1,27 @@
 public class quadrilatero {
 
-    public static void area(double lado) {
+    public static double area(double lado) {
 
         System.out.println("Área do quadrado: " + lado * lado);
-
-
-
+        return lado * lado;
     }
 
-
-    public static void area(double lado1, double lado2) {
+    public static double area(double lado1, double lado2) {
 
         System.out.println("Área do retângulo: " + lado1 * lado2);
-
-
+        return lado1 * lado2;
     }
 
-
-    public static void area(double baseMaior, double baseMenor, double altura) {
+    public static double area(double baseMaior, double baseMenor, double altura) {
 
         System.out.println("Área do trapézio: " + ((baseMenor+baseMaior)*altura) / 2);
-
-
+        return ((baseMenor+baseMaior)*altura) / 2;
     }
 
-    public static void area(float diagonal1, float diagonal2) {
+    public static float area(float diagonal1, float diagonal2) {
 
         System.out.println("Area do Losango: " + ((diagonal1 * diagonal2) / 2));
-
-
+        return ((diagonal1 * diagonal2) / 2);
     } 
 
 
